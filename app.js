c4b043fc37e51ceea19283a46b1b6257cb805b51
DivyaChinthala <divyachinthala519@gmail.com>
2024-08-21 07:38:11 +0530

    feat: setting headers for response

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ const html = fs.readFileSync("./index.html", "utf-8");
 const server = http.createServer((req, res) => {
   const path = req.url;
   if (path == "/" || path.toLowerCase() == "/home") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+      "my-custom-header": "Hello World!",
+    });
     res.end(html.replace("{{Content}}", "You are in Home page"));
   } else if (path.toLowerCase() == "/about") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+      "my-custom-header": "Hello World!",
+    });
     res.end(html.replace("{{Content}}", "You are in About page"));
   } else if (path.toLowerCase() == "/contact") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+      "my-custom-header": "Hello World!",
+    });
     res.end(html.replace("{{Content}}", "You are in Contact page"));
   } else {
+    res.writeHead(400, {
+      "Content-Type": "text/html",
+      "my-custom-header": "Hello World!",
+    });
     res.end(html.replace("{{Content}}", "Page not Found"));
   }
 });
