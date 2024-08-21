2cb4df26a491b5976f41b38ea7b8f272d39af3a8
DivyaChinthala <divyachinthala519@gmail.com>
2024-08-21 06:56:20 +0530

    feat: add routing description: Using Routing server can send different responses for different requests for the same endpoint.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello, we got your request");
-  console.log("Received a new request");
+  const path = req.url;
+  if (path == "/" || path.toLowerCase() == "/home") {
+    res.end("You are in Home page");
+  } else if (path.toLowerCase() == "/about") {
+    res.end("You are in About Page");
+  } else if (path.toLowerCase() == "/contact") {
+    res.end("You are in Contact Page");
+  } else {
+    res.end("Page not Found");
+  }
 });
 server.listen(9000, "localhost", () => {
   console.log("Server started listening......");
