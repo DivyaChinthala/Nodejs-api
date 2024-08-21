56b2b440674a04a8027f7b543bf72cb6328a4719
DivyaChinthala <divyachinthala519@gmail.com>
2024-08-21 07:34:59 +0530

    feat: send html response

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const http = require("http");
+const fs = require("fs");
 
+const html = fs.readFileSync("./index.html", "utf-8");
 const server = http.createServer((req, res) => {
   const path = req.url;
   if (path == "/" || path.toLowerCase() == "/home") {
-    res.end("You are in Home page");
+    res.end(html.replace("{{Content}}", "You are in Home page"));
   } else if (path.toLowerCase() == "/about") {
-    res.end("You are in About Page");
+    res.end(html.replace("{{Content}}", "You are in About page"));
   } else if (path.toLowerCase() == "/contact") {
-    res.end("You are in Contact Page");
+    res.end(html.replace("{{Content}}", "You are in Contact page"));
   } else {
-    res.end("Page not Found");
+    res.end(html.replace("{{Content}}", "Page not Found"));
   }
 });
 server.listen(9000, "localhost", () => {
