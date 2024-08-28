71a1c91170cabba59dd0d7d15f66d2f2b6c6510d
DivyaChinthala <divyachinthala519@gmail.com>
2024-08-28 19:15:24 +0530

    feat: use express route method for route handlers description: route method is used to define common route for different route handlers

--- app.js
+++ app.js
@@ -14,102 +14,108 @@ const logger = (req, res, next) => {
 app.listen(port, () => {
   console.log("Server started listening....");
 });
-
-app.get("/movies", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      movies: movies,
-    },
-  });
-});
-
 app.use(logger);
 
-app.post("/movies", (req, res) => {
-  const data = req.body;
-  const newMovie = {
-    id: movies.length + 1,
-    ...data,
-  };
-  const updatedMovies = [...movies, newMovie];
-  fs.writeFile("./movies.json", JSON.stringify(updatedMovies), (err) => {
+app
+  .route("/movies")
+  .get((req, res) => {
     res.status(200).json({
       status: "success",
       data: {
-        movie: newMovie,
+        movies: movies,
       },
     });
+  })
+  .post((req, res) => {
+    const data = req.body;
+    const newMovie = {
+      id: movies.length + 1,
+      ...data,
+    };
+    const updatedMovies = [...movies, newMovie];
+    fs.writeFile("./movies.json", JSON.stringify(updatedMovies), (err) => {
+      res.status(200).json({
+        status: "success",
+        data: {
+          movie: newMovie,
+        },
+      });
+    });
   });
-});
 
-app.get("/movies/:id", (req, res) => {
-  const params = req.params;
-  const existingMovie = movies.find((movie) => movie.id == params.id);
-  const existingMovieIndex = movies.findIndex((movie) => movie.id == params.id);
-  if (!existingMovie) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Movie With Id " + params.id + " not found",
+app
+  .route("/movies/:id")
+  .get((req, res) => {
+    const params = req.params;
+    const existingMovie = movies.find((movie) => movie.id == params.id);
+    const existingMovieIndex = movies.findIndex(
+      (movie) => movie.id == params.id
+    );
+    if (!existingMovie) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Movie With Id " + params.id + " not found",
+      });
+    }
+    const data = req.body;
+    const updatedMovie = {
+      ...existingMovie,
+      ...data,
+    };
+    movies[existingMovieIndex] = updatedMovie;
+    fs.writeFile("./movies.json", JSON.stringify(movies), (err) => {
+      res.status(200).json({
+        status: "success",
+        data: {
+          movie: updatedMovie,
+        },
+      });
     });
-  }
-  const data = req.body;
-  const updatedMovie = {
-    ...existingMovie,
-    ...data,
-  };
-  movies[existingMovieIndex] = updatedMovie;
-  fs.writeFile("./movies.json", JSON.stringify(movies), (err) => {
-    res.status(200).json({
-      status: "success",
-      data: {
-        movie: updatedMovie,
-      },
+  })
+  .patch((req, res) => {
+    const params = req.params;
+    const existingMovie = movies.find((movie) => movie.id == params.id);
+    const existingMovieIndex = movies.findIndex(
+      (movie) => movie.id == params.id
+    );
+    if (!existingMovie) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Movie With Id " + params.id + " not found",
+      });
+    }
+    const data = req.body;
+    const updatedMovie = {
+      ...existingMovie,
+      ...data,
+    };
+    movies[existingMovieIndex] = updatedMovie;
+    fs.writeFile("./movies.json", JSON.stringify(movies), (err) => {
+      res.status(200).json({
+        status: "success",
+        data: {
+          movie: updatedMovie,
+        },
+      });
+    });
+  })
+  .delete((req, res) => {
+    const params = req.params;
+    const existingMovie = movies.find((movie) => movie.id == params.id);
+    const existingMovieIndex = movies.findIndex(
+      (movie) => movie.id == params.id
+    );
+    if (!existingMovie) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Movie With Id " + params.id + " not found",
+      });
+    }
+    movies.splice(existingMovieIndex, 1);
+    fs.writeFile("./movies.json", JSON.stringify(movies), (err) => {
+      res.status(200).json({
+        status: "success",
+        message: "Movie Deleted Successfully",
+      });
     });
   });
-});
-
-app.patch("/movies/:id", (req, res) => {
-  const params = req.params;
-  const existingMovie = movies.find((movie) => movie.id == params.id);
-  const existingMovieIndex = movies.findIndex((movie) => movie.id == params.id);
-  if (!existingMovie) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Movie With Id " + params.id + " not found",
-    });
-  }
-  const data = req.body;
-  const updatedMovie = {
-    ...existingMovie,
-    ...data,
-  };
-  movies[existingMovieIndex] = updatedMovie;
-  fs.writeFile("./movies.json", JSON.stringify(movies), (err) => {
-    res.status(200).json({
-      status: "success",
-      data: {
-        movie: updatedMovie,
-      },
-    });
-  });
-});
-
-app.delete("/movies/:id", (req, res) => {
-  const params = req.params;
-  const existingMovie = movies.find((movie) => movie.id == params.id);
-  const existingMovieIndex = movies.findIndex((movie) => movie.id == params.id);
-  if (!existingMovie) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Movie With Id " + params.id + " not found",
-    });
-  }
-  movies.splice(existingMovieIndex, 1);
-  fs.writeFile("./movies.json", JSON.stringify(movies), (err) => {
-    res.status(200).json({
-      status: "success",
-      message: "Movie Deleted Successfully",
-    });
-  });
-});
